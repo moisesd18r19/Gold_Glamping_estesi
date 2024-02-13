42384dfd4238399d5905ba9e9ed7c136138cb6b4
Higuita2398 <higuitasamuel9@gmail.com>
2024-02-12 21:45:28 -0500

    arreglos

--- Glamping_Gold/Glamping_Gold/views.py
+++ Glamping_Gold/Glamping_Gold/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-def login (request):
-    return render(request, 'login')
+def login(request):
+    return render(request, 'login.html')
